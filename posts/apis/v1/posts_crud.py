4a20595b291c020eefef3abe772dbafd11f5c9d6
Ryzen <sosnovscky.nikita@yandex.ru>
2022-01-24 15:52:37 +0300

    Posts finished crud operations + swagger documentation

--- posts/apis/v1/posts_crud.py
+++ posts/apis/v1/posts_crud.py
@@ -51,7 +51,45 @@ def create_post(user_id):
 @connex_app.route('/api/users/<int:user_id>/posts/<int:post_id>', methods=['PUT'])
 def update_post(user_id, post_id):
     post = request.get_json(force=True)
+    title = post.get('title')
+    body = post.get('body')
+    imageUrl = post.get('imageUrl')
+    updated_post = (
+        Post.query.filter(Post.author_id == user_id)
+        .filter(Post.post_id == post_id)
+        .one_or_none()
+    )
+    if updated_post is not None:
+        update_schema = PostSchema()
+        updating_post = update_schema.load(post, session=db.session).data
 
+        updating_post.author_id = updated_post.author_id
+        updating_post.post_id = updated_post.post_id
+
+        db.session.merge(updating_post)
+        db.session.commit()
+
+        data = update_schema.dumps(updated_post).data
+        return data, 200
+    else:
+        abort(404, f"Post with Id:{post_id} is not found...")
+
+
+@connex_app.route('/api/users/<int:user_id>/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(user_id, post_id):
+    post = (
+        Post.query.filter(User.user_id == user_id)
+        .filter(Post.post_id == post_id)
+        .one_or_none()
+    )
+    if post is not None:
+        db.session.delete(post)
+        db.session.commit()
+        return make_response(
+            f"Post with Id:{post_id} deleted successfully...", 200
+        )
+    else:
+        abort(404, f"Post with Id:{post_id} is not found...")
 
 
 
